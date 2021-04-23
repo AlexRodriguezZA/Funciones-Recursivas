d7989b7099534edec15468a5b6fe8723c8fcb8c1
alex <alekeyn1999@gmail.com>
2021-04-23 19:16:12 -0300

    ejer12

--- Ejer12.py
+++ Ejer12.py
@@ -1,3 +1,15 @@
 #Desarrollar el algoritmo de Euclides para calcular el máximo común divisor (MCD) de dos
-#número entero.
+#números enteros
 
+#24 % 16 ---> [8] --> 16 % 8 --> [0]
+
+def EuclidesMCD(numero1,numero2):
+    if numero1>numero2:
+
+        if (numero1 % numero2 == 0):
+             print("El MCD es -->  ", numero2)
+        else:
+            return EuclidesMCD(numero1,numero1%numero2)
+
+
+print(EuclidesMCD(78,30))
